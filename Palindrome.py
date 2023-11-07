1775351f0a1002279f5e93f3dd9004ca030a7408
pritom <pritomdip010@gmail.com>
2023-11-07 14:38:29 +0000

    Sort word at alphabetic order

--- Palindrome.py
+++ Palindrome.py
@@ -33,3 +33,25 @@ for char in my_str:
 # display the unpunctuated string
 print(no_punct)
 
+
+
+
+# Program to sort alphabetically the words form a string provided by the user
+
+my_str = "Hello this Is an Example With cased letters"
+
+# To take input from the user
+#my_str = input("Enter a string: ")
+
+# breakdown the string into a list of words
+words = [word.lower() for word in my_str.split()]
+
+# sort the list
+words.sort()
+
+# display the sorted words
+
+print("The sorted words are:")
+for word in words:
+   print(word)
+
