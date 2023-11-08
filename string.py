6bd75a95f6fc6fa6dec5647c81e1a61f1c9892b7
pritom <pritomdip010@gmail.com>
2023-11-08 11:49:00 +0000

    Get substring of a string

--- string.py
+++ string.py
@@ -2,3 +2,15 @@ my_list = ['a', 'b', 'c', 'd', 'e']
 
 # print the last element
 print(my_list[-1])
+
+
+my_string = "I love python."
+
+# prints "love"
+print(my_string[2:6])
+
+# prints "love python."
+print(my_string[2:])
+
+# prints "I love python"
+print(my_string[:-1])
