3446d2f8935b5a56d562aa05395854c96431bf05
pritom <pritomdip010@gmail.com>
2023-11-09 23:06:46 +0000

    printing my list9

--- new2.py
+++ new2.py
@@ -18,6 +18,4 @@ my_list = [1, 'a', 32, 'c', 'd', 31]
 print(secrets.choice(my_list))
 my_list = [1, 'a', 32, 'c', 'd', 31]
 print(random.choice(my_list))
-my_list = [1, 'a', 32, 'c', 'd', 31]
-print(secrets.choice(my_list))
 
