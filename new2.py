ca1d67ed57291dd9c5c0a22ac0945f98feb1fcc2
pritom <pritomdip010@gmail.com>
2023-11-09 23:05:30 +0000

    printing my list7

--- new2.py
+++ new2.py
@@ -1,21 +1,6 @@
 import random
 import secrets
 
-my_list = [1, 'a', 32, 'c', 'd', 31]
-print(random.choice(my_list))
-my_list = [1, 'a', 32, 'c', 'd', 31]
-print(secrets.choice(my_list))
-my_list = [1, 'a', 32, 'c', 'd', 31]
-print(random.choice(my_list))
-my_list = [1, 'a', 32, 'c', 'd', 31]
-print(secrets.choice(my_list))
-my_list = [1, 'a', 32, 'c', 'd', 31]
-print(random.choice(my_list))
-my_list = [1, 'a', 32, 'c', 'd', 31]
-print(secrets.choice(my_list))
-my_list = [1, 'a', 32, 'c', 'd', 31]
-print(random.choice(my_list))
-my_list = [1, 'a', 32, 'c', 'd', 31]
-print(secrets.choice(my_list))
+
 
 
