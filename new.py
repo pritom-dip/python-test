3a56781a202ef2dee5b511c7670813da27d8d24a
pritom <pritomdip010@gmail.com>
2023-11-09 22:56:42 +0000

    printing my list

--- new.py
+++ new.py
@@ -5,3 +5,6 @@ my_list = [1, 'a', 32, 'c', 'd', 31]
 print(random.choice(my_list))
 my_list = [1, 'a', 32, 'c', 'd', 31]
 print(secrets.choice(my_list))
+
+my_list1 = [1, 'a', 32, 'b', 'c', 31]
+print(secrets.choice(my_list1))
