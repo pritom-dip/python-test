3f936d225cddc2fe43d369de441046dc33ee367e
pritom <pritomdip010@gmail.com>
2023-11-09 23:01:07 +0000

    printing my list3

--- new.py
+++ new.py
@@ -8,3 +8,9 @@ print(secrets.choice(my_list))
 
 my_list1 = [1, 'a', 32, 'b', 'c', 31]
 print(secrets.choice(my_list1))
+
+my_list2 = [2, 'a', 33, 'b', 'c', 56]
+print(secrets.choice(my_list2))
+
+my_list11 = [8, 'a', 32, '78', 'c', 31]
+print(secrets.choice(my_list11))
