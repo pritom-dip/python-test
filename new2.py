381f0eba694b8f8366f1da42bb40746454a80fc7
pritom <pritomdip010@gmail.com>
2023-11-09 23:03:57 +0000

    printing my list5

--- new2.py
+++ new2.py
@@ -6,9 +6,4 @@ print(random.choice(my_list))
 my_list = [1, 'a', 32, 'c', 'd', 31]
 print(secrets.choice(my_list))
 
-my_list1 = [1, 'a', 32, 'b', 'c', 31]
-print(secrets.choice(my_list1))
-
-my_list2 = [2, 'a', 33, 'b', 'c', 56]
-print(secrets.choice(my_list2))
 
