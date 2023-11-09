7325324305ae43b1f6233f7859e8d13da39b6206
pritom <pritomdip010@gmail.com>
2023-11-09 23:02:28 +0000

    printing my lis3

---
+++ new2.py
@@ -0,0 +1,16 @@
+import random
+import secrets
+
+my_list = [1, 'a', 32, 'c', 'd', 31]
+print(random.choice(my_list))
+my_list = [1, 'a', 32, 'c', 'd', 31]
+print(secrets.choice(my_list))
+
+my_list1 = [1, 'a', 32, 'b', 'c', 31]
+print(secrets.choice(my_list1))
+
+my_list2 = [2, 'a', 33, 'b', 'c', 56]
+print(secrets.choice(my_list2))
+
+my_list11 = [8, 'a', 32, '78', 'c', 31]
+print(secrets.choice(my_list11))
