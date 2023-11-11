5a5707192f993e6c942257a39e04ba19edf5d190
pritom <pritomdip010@gmail.com>
2023-11-11 18:47:03 +0000

    comment3

--- greater.py
+++ greater.py
@@ -10,9 +10,4 @@ if 5 > 2:
         print("Five is greater than two!")
 
         print("Hello, World!") #This is a commentif 5 > 2:
-        
- print("Five is greater than two!")  
-if 5 > 2:
-        print("Five is greater than two!")
 
-        print("Hello, World!") #This is a comment
