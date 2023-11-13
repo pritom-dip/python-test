75e00b63be7885eb8811defd8b1ae649c81e58c4
pritom <pritomdip010@gmail.com>
2023-11-13 19:54:56 +0000

    oper

--- op.py
+++ op.py
@@ -5,3 +5,9 @@ print((6 + 3) - (6 + 3))
 print(100 + 5 * 3)
 
 print(5 + 4 - 7 + 8)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
