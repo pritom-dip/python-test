8d6ba06f74d3254f57b473caee22870f8fe42a4d
pritom <pritomdip010@gmail.com>
2023-11-15 22:04:37 +0000

    String permutation

--- time.py
+++ time.py
@@ -11,3 +11,21 @@ def countdown(time_sec):
     print("stop")
 
 countdown(5)
+
+
+
+def get_permutation(string, i=0):
+
+    if i == len(string):   	 
+        print("".join(string))
+
+    for j in range(i, len(string)):
+
+        words = [c for c in string]
+   
+        # swap
+        words[i], words[j] = words[j], words[i]
+   	 
+        get_permutation(words, i + 1)
+
+print(get_permutation('yup'))
